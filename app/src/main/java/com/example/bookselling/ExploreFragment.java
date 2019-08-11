eca5d8124cc791d6d5e93c0493edb9d498741a8f
abhishekk673 <Suryaabhishek804@gmail.com>
2019-08-11 23:02:06 +0530

    long click on recycler view items implemented

--- app/src/main/java/com/example/bookselling/ExploreFragment.java
+++ app/src/main/java/com/example/bookselling/ExploreFragment.java
@@ -15,6 +15,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
@@ -126,6 +127,12 @@ Log.i("abc","Oncreateview");
         intent.putExtra("position",position);
         startActivity(intent);
     }
+
+    @Override
+    public void OnBookLongClick(int position) {
+        Toast.makeText(getContext(),"long clik"+position,Toast.LENGTH_SHORT).show();
+    }
+
 
 }
 
